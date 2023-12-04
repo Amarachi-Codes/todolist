b25c99c10bac87089e9f91aaa9bb2e7fc76a0099
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-04 23:33:35 +0100

    shoping list

--- shoppinglist.js
+++ shoppinglist.js
@@ -1,18 +1,42 @@
-const input=document.getElementById("input").value.trim()
-const button =document.getElementById("enter")
+// const input=document.getElementById("input").value.trim()
+// const button =document.getElementById("enter")
+// const listItems = document.getElementById("listItems")
 
-const listItems = document.getElementById("listItems")
+// button.addEventListener("click",function (){
+     
+// additems(input)
 
-button.addEventListener("click",()=>{
+// })
 
-})
+// function additems(input){
+//     const li =document.createElement("li")
+   
+//     if(input !== ""){
+//          li.innerHTML=`
+//         <li>${input}</li>`
+//     }else{
+//         return "input cannot be empty"
+//     }
+// }
 
-function additems(input){
-    const li =document.createElement("li")
-    if(input !== ""){
-         li.innerHTML=`
-        <li>${input}</li>`
-    }else{
-        return "input cannot be empty"
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById("enter");
+
+    button.addEventListener("click", function () {
+        const input = document.getElementById("input").value.trim();
+        const listItems = document.getElementById("listItems");
+
+        if (input !== "") {
+            addItems(input, listItems);
+            document.getElementById("input").value = ''; // Clear input field
+        } else {
+            alert("Input cannot be empty");
+        }
+    });
+
+    function addItems(input, listItems) {
+        const li = document.createElement("li");
+        li.textContent = input;
+        listItems.appendChild(li);
     }
-}
+});
